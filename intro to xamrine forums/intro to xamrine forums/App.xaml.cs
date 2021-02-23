b574b25d9da9cda2c81a70c67cae2e3594af2abb
engkhkh <khilel208@gmail.com>
2021-02-23 14:32:48 +0300

    Add project files.

---
+++ intro to xamrine forums/intro to xamrine forums/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using intro_to_xamrine_forums;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace intro_to_xamrine_forums
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MyPage();
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
